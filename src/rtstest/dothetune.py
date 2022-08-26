d761adb1b84546bad86852797afafac1ba652f01
Kilian Lieret <kilian.lieret@posteo.de>
2022-08-26 13:37:55 -0400

    ✨ Add tutorial code

---
+++ src/rtstest/dothetune.py
@@ -0,0 +1,122 @@
+# Based on https://docs.ray.io/en/master/tune/getting-started.html#tune-tutorial
+
+from __future__ import annotations
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from ray import tune
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # In this example, we don't change the model architecture
+        # due to simplicity.
+        self.conv1 = nn.Conv2d(1, 3, kernel_size=3)
+        self.fc = nn.Linear(192, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 3))
+        x = x.view(-1, 192)
+        x = self.fc(x)
+        return F.log_softmax(x, dim=1)
+
+
+# Change these values if you want the training to run quicker or slower.
+EPOCH_SIZE = 512
+TEST_SIZE = 256
+
+
+def train(model, optimizer, train_loader):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        # We set this just for the example to run quickly.
+        if batch_idx * len(data) > EPOCH_SIZE:
+            return
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+
+def test(model, data_loader):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(data_loader):
+            # We set this just for the example to run quickly.
+            if batch_idx * len(data) > TEST_SIZE:
+                break
+            data, target = data.to(device), target.to(device)
+            outputs = model(data)
+            _, predicted = torch.max(outputs.data, 1)
+            total += target.size(0)
+            correct += (predicted == target).sum().item()
+
+    return correct / total
+
+
+def train_mnist(config):
+    # Data Setup
+    mnist_transforms = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    )
+
+    train_loader = DataLoader(
+        datasets.MNIST("./data", train=True, download=True, transform=mnist_transforms),
+        batch_size=64,
+        shuffle=True,
+    )
+    test_loader = DataLoader(
+        datasets.MNIST("./data", train=False, transform=mnist_transforms),
+        batch_size=64,
+        shuffle=True,
+    )
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = ConvNet()
+    model.to(device)
+
+    optimizer = optim.SGD(
+        model.parameters(), lr=config["lr"], momentum=config["momentum"]
+    )
+    for i in range(10):
+        train(model, optimizer, train_loader)
+        acc = test(model, test_loader)
+
+        # Send the current training result back to Tune
+        tune.report(mean_accuracy=acc)
+
+        if i % 5 == 0:
+            # This saves the model to the trial directory
+            torch.save(model.state_dict(), "./model.pth")
+
+
+if __name__ == "__main__":
+    search_space = {
+        "lr": tune.sample_from(lambda spec: 10 ** (-10 * np.random.rand())),
+        "momentum": tune.uniform(0.1, 0.9),
+    }
+
+    # Uncomment this to enable distributed execution
+    # `ray.init(address="auto")`
+
+    # Download the dataset first
+    datasets.MNIST("~/data", train=True, download=True)
+
+    tuner = tune.Tuner(
+        train_mnist,
+        param_space=search_space,
+    )
+    results = tuner.fit()
